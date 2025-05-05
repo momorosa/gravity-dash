4d78045c7ec5df4dc9db0f82d99d5b13a796dd27
Rosa Choi <122864464+momorosa@users.noreply.github.com>
2025-05-04 20:36:51 -0700

    Add random obstacle block generation and customizable level length

--- src/components/Level.jsx
+++ src/components/Level.jsx
@@ -1,15 +1,23 @@
+import { useMemo } from 'react'
 import { BlockStart, BlockSpinner, BlockLimbo, BlockAxe, BlockStepper, BlockEnd } from './blocks.js'
 
-
-
-export default function Level()
+export default function Level({  count = 7, types = [ BlockSpinner, BlockLimbo, BlockAxe, BlockStepper ] })
 {
+    const blocks = useMemo(() => 
+    {
+        const blocks = []
+        
+        for (let i = 0; i < count; i++)
+        {
+            const type = types[ Math.floor(Math.random() * types.length) ]
+            blocks.push(type)
+        }
+        return blocks
+    }, [ count, types ])
+
     return <>
-         <BlockStart position={ [ 0, 0, 20 ] }/>
-         <BlockSpinner position={ [ 0, 0, 16 ] }/>
-         <BlockLimbo position={ [ 0, 0, 12 ] }/>
-         <BlockAxe position={ [ 0, 0, 8 ] }/>
-         <BlockStepper position={ [ 0, 0, 4 ] }/>
-         <BlockEnd position={ [ 0, 0, 0 ] }/>      
+        <BlockStart position={ [ 0, 0, 0 ] }/>
+        { blocks.map((Block, index) => <Block key={ index } position={ [ 0, 0, -(index + 1) * 4 ] } /> )}
+        <BlockEnd position={ [ 0, 0, -(count + 1) * 4 ] } />
     </>
 }
